b0fea09640f49607fb29cee3a28407c623eac06e
coni <coni.palavecino@gmail.com>
2023-08-28 21:19:08 -0400

    add carousel

--- src/components/ViewProduct/ViewProductComponent.jsx
+++ src/components/ViewProduct/ViewProductComponent.jsx
@@ -1,4 +1,6 @@
-import { Card, ListGroup } from "react-bootstrap";
+import { useState } from "react";
+// import { Card, Carousel, ListGroup } from "react-bootstrap";
+import Carousel from 'react-bootstrap/Carousel';
 import { useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
@@ -10,6 +12,13 @@ const ViewProductComponent = () => {
   const receivedProps = location.state;
   const navigateTo = useNavigate();
 
+
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
   const goViewProduct = () => {
     navigateTo("/ProductsList");
   };
@@ -17,30 +26,37 @@ const ViewProductComponent = () => {
   return (
     <>
       {receivedProps ? (
-        <Card className="vcard-product">
-          <Card.Title>{receivedProps.name}</Card.Title>
-          <Card.Img
-            className="vcard-product-img"
-            variant="top"
-            src={receivedProps.img}
-          />
-          <Card.Body>
-            <Card.Text>{receivedProps.description}</Card.Text>
-          </Card.Body>
-          <ListGroup className="list-group-flush">
-            <ListGroup.Item className="vprice-img">
-              ${receivedProps.price}
-            </ListGroup.Item>
-          </ListGroup>
-          <Card.Body>
-            <button
-              className="btn form-control"
-              onClick={goViewProduct}
-            >
-              volver
-            </button>
-          </Card.Body>
-        </Card>
+        <>
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+      <img src={receivedProps.img} alt="" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src={receivedProps.img} alt="" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src={receivedProps.img} alt="" />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+        
+          <button className="btn form-control" onClick={goViewProduct}>
+            volver
+          </button>
+        </>
       ) : (
         navigateTo("/ProductsList")
       )}
@@ -49,3 +65,28 @@ const ViewProductComponent = () => {
 };
 
 export default ViewProductComponent;
+
+// <Card className="vcard-product">
+//   <Card.Title>{receivedProps.name}</Card.Title>
+//   <Card.Img
+//     className="vcard-product-img"
+//     variant="top"
+//     src={receivedProps.img}
+//   />
+//   <Card.Body>
+//     <Card.Text>{receivedProps.description}</Card.Text>
+//   </Card.Body>
+//   <ListGroup className="list-group-flush">
+//     <ListGroup.Item className="vprice-img">
+//       ${receivedProps.price}
+//     </ListGroup.Item>
+//   </ListGroup>
+//   <Card.Body>
+//     <button
+//       className="btn form-control"
+//       onClick={goViewProduct}
+//     >
+//       volver
+//     </button>
+//   </Card.Body>
+// </Card>
